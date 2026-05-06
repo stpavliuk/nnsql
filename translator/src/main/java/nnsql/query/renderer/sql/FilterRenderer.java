283b8281d35b999d4757f8854c8553541c8a4e0c
Stanislav Pavliuk <stanislavpavlyk@gmail.com>
2026-05-06 06:02:20 +0300

    Anchor filter passthrough attributes on ids

--- translator/src/main/java/nnsql/query/renderer/sql/FilterRenderer.java
+++ translator/src/main/java/nnsql/query/renderer/sql/FilterRenderer.java
@@ -3,6 +3,8 @@ package nnsql.query.renderer.sql;
 import nnsql.query.ir.Filter;
 import nnsql.query.renderer.RenderContext;
 
+import java.util.List;
+
 import static nnsql.query.renderer.sql.Sql.*;
 
 record FilterRenderer(ConditionRenderer conditionRenderer) {
@@ -14,6 +16,29 @@ record FilterRenderer(ConditionRenderer conditionRenderer) {
                 () -> addFilterIdCTE(ctx, baseName, inputBaseName,
                     conditionRenderer.renderTrue(filter.condition(), inputBaseName, ctx))
             );
-        addPassthroughAttributeCTEs(ctx, baseName, inputBaseName, filter.attributes(), Sql::attrTable);
+        addSelectivePassthroughAttributeCTEs(ctx, baseName, inputBaseName, filter.attributes());
+    }
+
+    private void addSelectivePassthroughAttributeCTEs(
+        RenderContext ctx,
+        String baseName,
+        String inputBaseName,
+        List<String> attributes
+    ) {
+        attributes.forEach(attr -> {
+            var inputAttrTbl = table(attrTable(inputBaseName, attr));
+            var baseIdTbl = table(idTable(baseName));
+            var ps = new net.sf.jsqlparser.statement.select.PlainSelect();
+            ps.addSelectItem(new net.sf.jsqlparser.statement.select.AllTableColumns(inputAttrTbl));
+            ps.setFromItem(baseIdTbl);
+            ps.addJoins(join(
+                inputAttrTbl,
+                new net.sf.jsqlparser.expression.operators.relational.EqualsTo(
+                    column(inputAttrTbl, "id"),
+                    column(baseIdTbl, "id")
+                )
+            ));
+            ctx.addCTE(attrTable(baseName, attr), ps);
+        });
     }
 }
